a18b582313f9d27c014c02f9ce7b6fc17f194b56
ahmedtvmer <ahmedhegazy4me@gmail.com>
2025-01-14 08:39:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextDataAnalayzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
